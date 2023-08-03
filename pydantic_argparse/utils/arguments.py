4c364b23362ffff33f6c843b29255670c2e57fa5
cody-mar10 <ccmartin6@wisc.edu>
2023-08-03 16:01:25 -0500

    pydantic v2 updates and switched metavar to type

--- pydantic_argparse/utils/arguments.py
+++ pydantic_argparse/utils/arguments.py
@@ -4,16 +4,16 @@ The `arguments` module contains utility functions used for formatting argument
 names and formatting argument descriptions.
 """
 
+from typing import Optional, get_args
 
-# Third-Party
-import pydantic
+from .pydantic import PydanticField
 
 
-def name(field: pydantic.fields.ModelField, invert: bool = False) -> str:
+def name(field: PydanticField, invert: bool = False) -> str:
     """Standardises argument name.
 
     Args:
-        field (pydantic.fields.ModelField): Field to construct name for.
+        field (PydanticField): Field to construct name for.
         invert (bool): Whether to invert the name by prepending `--no-`.
 
     Returns:
@@ -21,22 +21,50 @@ def name(field: pydantic.fields.ModelField, invert: bool = False) -> str:
     """
     # Construct Prefix
     prefix = "--no-" if invert else "--"
+    name = field.info.title or field.name
 
     # Prepend prefix, replace '_' with '-'
-    return f"{prefix}{field.alias.replace('_', '-')}"
+    return f"{prefix}{name.replace('_', '-')}"
 
 
-def description(field: pydantic.fields.ModelField) -> str:
+def description(field: PydanticField) -> str:
     """Standardises argument description.
 
     Args:
-        field (pydantic.fields.ModelField): Field to construct description for.
+        field (PydanticField): Field to construct description for.
 
     Returns:
         str: Standardised description of the argument.
     """
     # Construct Default String
-    default = f"(default: {field.get_default()})" if not field.required else None
+    default = (
+        f"(default: {field.info.get_default()})"
+        if not field.info.is_required()
+        else None
+    )
 
     # Return Standardised Description String
-    return " ".join(filter(None, [field.field_info.description, default]))
+    return " ".join(filter(None, [field.info.description, default]))
+
+
+def metavar(field: PydanticField) -> Optional[str]:
+    """Generate the metavar name for the field.
+
+    Args:
+        field (PydanticField): Field to construct metavar for.
+
+    Returns:
+        Optional[str]: Field metavar if the `Field.info.alias` exists
+    """
+    # metavar is the type
+    if field.info.annotation is not None:
+        inner_types = get_args(field.info.annotation)
+        if inner_types:
+            return inner_types[0].__name__.upper()
+        return field.info.annotation.__name__.upper()
+
+    # this is pretty much unreachable
+    if field.info.alias is not None:
+        return field.info.alias.upper()
+
+    return
