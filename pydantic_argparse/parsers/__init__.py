2fe5a0c9bd29be35a45b4e2603cbf92ab87104f8
cody-mar10 <ccmartin6@wisc.edu>
2023-08-03 16:04:24 -0500

    moved field type switch out of parser loop to here

--- pydantic_argparse/parsers/__init__.py
+++ pydantic_argparse/parsers/__init__.py
@@ -7,11 +7,51 @@ The public interface exposed by this package is the `parsing` modules, which
 each contain the `should_parse()` and `parse_field()` functions.
 """
 
-# Local
-from pydantic_argparse.parsers import boolean
-from pydantic_argparse.parsers import command
-from pydantic_argparse.parsers import container
-from pydantic_argparse.parsers import enum
-from pydantic_argparse.parsers import literal
-from pydantic_argparse.parsers import mapping
-from pydantic_argparse.parsers import standard
+from typing import Optional
+
+from pydantic_argparse.utils.pydantic import PydanticField, PydanticValidator
+
+from . import (
+    boolean,
+    command,
+    container,
+    enum,
+    literal,
+    mapping,
+    standard,
+)
+from .utils import SupportsAddArgument
+
+
+def add_field(
+    parser: SupportsAddArgument,
+    field: PydanticField,
+) -> Optional[PydanticValidator]:
+    """Parses pydantic field type, and then adds it to argument parser.
+
+    Args:
+        parser (argparse.ArgumentParser | argparse._ArgumentGroup): Sub-parser to add to.
+        field (pydantic.fields.ModelField): Field to be added to parser.
+
+    Returns:
+        Optional[utils.pydantic.PydanticValidator]: Possible validator method.
+    """
+    # Switch on Field Type -- for fields that are pydantic models
+    # this gets handled at the top level to distinguish
+    # subcommands from arg groups
+    if boolean.should_parse(field):
+        return boolean.parse_field(parser, field)
+
+    if container.should_parse(field):
+        return container.parse_field(parser, field)
+
+    if mapping.should_parse(field):
+        return mapping.parse_field(parser, field)
+
+    if literal.should_parse(field):
+        return literal.parse_field(parser, field)
+
+    if enum.should_parse(field):
+        return enum.parse_field(parser, field)
+
+    return standard.parse_field(parser, field)
