1d2439385c116dc91aa25203a955e7abe40c47d4
cody-mar10 <ccmartin6@wisc.edu>
2023-08-07 12:55:28 -0500

    removed functions that referred to `PydanticField`
      - these were moved to methods of the class

--- pydantic_argparse/utils/types.py
+++ pydantic_argparse/utils/types.py
@@ -6,54 +6,22 @@ comparing the types of `pydantic fields.
 
 
 import sys
-from typing import Any, Tuple, Union
-
-from .pydantic import PydanticField
+from typing import Iterable
 
 # Version-Guarded
 if sys.version_info < (3, 8):  # pragma: <3.8 cover
-    from typing_extensions import get_origin
+    pass
 else:  # pragma: >=3.8 cover
-    from typing import get_origin
+    pass
 
 
-def is_field_a(
-    field: PydanticField,
-    types: Union[Any, Tuple[Any, ...]],
-) -> bool:
-    """Checks whether the subject *is* any of the supplied types.
-
-    The checks are performed as follows:
-
-    1. `field` *is* one of the `types`
-    2. `field` *is an instance* of one of the `types`
-    3. `field` *is a subclass* of one of the `types`
-
-    If any of these conditions are `True`, then the function returns `True`,
-    else `False`.
+def all_types(types: Iterable) -> bool:
+    """Check if all inputs are `type`s and not instances.
 
     Args:
-        field (PydanticField): Subject field to check type of.
-        types (Union[Any, Tuple[Any, ...]]): Type(s) to compare field against.
+        types (Iterable): an interable of putative `type` objects
 
     Returns:
-        bool: Whether the field *is* considered one of the types.
+        bool: whether or not all inputs are `type`s
     """
-    # Create tuple if only one type was provided
-    if not isinstance(types, tuple):
-        types = (types,)
-
-    # Get field type, or origin if applicable
-    field_type = get_origin(field.info.annotation) or field.info.annotation
-
-    # Check `isinstance` and `issubclass` validity
-    # In order for `isinstance` and `issubclass` to be valid, all arguments
-    # should be instances of `type`, otherwise `TypeError` *may* be raised.
-    is_valid = all(isinstance(t, type) for t in (*types, field_type))
-
-    # Perform checks and return
-    return (
-        field_type in types
-        or (is_valid and isinstance(field_type, types))
-        or (is_valid and issubclass(field_type, types))
-    )
+    return all(isinstance(t, type) for t in types)
