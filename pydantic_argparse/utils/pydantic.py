eb257ede213d3c0b0b2029d2971619c7365e8c11
cody-mar10 <ccmartin6@wisc.edu>
2023-08-07 12:53:18 -0500

    change field code to methods of `PydanticField`
      - the following are now directly coupled to the class def:
        - type parsing, including inner, outer, and main types
        - pydantic model type
        - type comparisons with `PydanticField.is_a` method
        - subcommand checks with `PydanticField.is_subcommand`

--- pydantic_argparse/utils/pydantic.py
+++ pydantic_argparse/utils/pydantic.py
@@ -6,27 +6,35 @@ field validator dictionaries and constructing new model classes with
 dynamically generated validators and environment variable parsers.
 """
 
+from collections.abc import Container, Mapping
 from typing import (
     Any,
     Callable,
     Dict,
     Iterator,
+    Literal,
     NamedTuple,
     Optional,
+    Tuple,
     Type,
     TypeVar,
     Union,
     cast,
+    get_args,
+    get_origin,
 )
 
 import pydantic
 from pydantic import BaseModel
 from pydantic.fields import FieldInfo
 
+from .types import all_types
+
 # Constants
 T = TypeVar("T")
 PydanticModelT = TypeVar("PydanticModelT", bound=BaseModel)
 PydanticValidator = classmethod
+NoneType = type(None)
 
 
 class PydanticField(NamedTuple):
@@ -34,6 +42,8 @@ class PydanticField(NamedTuple):
 
     Pydantic fields no longer store their name, so this named tuple
     keeps the field name and field info together.
+
+    The recommended entry point for an arbitrary `pydantic.BaseModel` is the classmethod `PydanticField.parse_model`.
     """
 
     name: str
@@ -46,10 +56,154 @@ class PydanticField(NamedTuple):
         """Iterator over the pydantic model fields, yielding this wrapper class.
 
         Yields:
-            `PydanticField`
+            Instances of self (`PydanticField`)
         """
         for name, info in model.model_fields.items():
             yield cls(name, info)
+
+    @property
+    def outer_type(self) -> Optional[Type]:
+        """Returns the outer type for nested types using `typing.get_origin`.
+
+        This will return `None` for simple, unnested types.
+        """
+        return get_origin(self.info.annotation)
+
+    @property
+    def inner_type(self) -> Tuple[Type, ...]:
+        """Returns the inner type for nested types using `typing.get_args`.
+
+        This will be an empty tuple for simple, unnested types.
+        """
+        return get_args(self.info.annotation)
+
+    @property
+    def main_type(self) -> Tuple[Type, ...]:
+        """Return the main inner types.
+
+        This excludes the `NoneType` when dealing with `typing.Optional` types.
+        """
+        return tuple(t for t in self.inner_type if t is not NoneType)
+
+    def get_type(self) -> Union[Type, Tuple[Type, ...], None]:
+        """Return the type annotation for the `pydantic` field.
+
+        Returns:
+            Union[Type, Tuple[Type, ...], None]
+        """
+        field_type = self.info.annotation
+        main_type = self.main_type
+        outer_type = self.outer_type
+        outer_type_is_type = isinstance(outer_type, type)
+        if outer_type and (
+            isinstance(outer_type, (Container, Mapping))
+            or (outer_type_is_type and issubclass(outer_type, (Container, Mapping)))
+            or outer_type is Literal
+        ):
+            # only return if outer_type is a concrete type like list, dict, etc OR typing.Literal
+            # NOT if outer_type is typing.Union, etc
+            return outer_type
+        if main_type and all_types(main_type):
+            # the all type check is specifically for typing.Literal
+            return main_type
+        return field_type
+
+    def is_a(self, types: Union[Any, Tuple[Any, ...]]) -> bool:
+        """Checks whether the subject *is* any of the supplied types.
+
+        The checks are performed as follows:
+
+        1. `field` *is* one of the `types`
+        2. `field` *is an instance* of one of the `types`
+        3. `field` *is a subclass* of one of the `types`
+
+        If any of these conditions are `True`, then the function returns `True`,
+        else `False`.
+
+        Args:
+            types (Union[Any, Tuple[Any, ...]]): Type(s) to compare field against.
+
+        Returns:
+            bool: Whether the field *is* considered one of the types.
+        """
+        # Create tuple if only one type was provided
+        if not isinstance(types, tuple):
+            types = (types,)
+
+        # Get field type, or origin if applicable
+        field_type = self.get_type()
+        if not isinstance(field_type, tuple):
+            field_type = (field_type,)
+
+        # Check `isinstance` and `issubclass` validity
+        # In order for `isinstance` and `issubclass` to be valid, all arguments
+        # should be instances of `type`, otherwise `TypeError` *may* be raised.
+
+        is_valid = all_types((*types, *field_type))
+
+        # Perform checks and return
+        is_type = False
+        for t in field_type:
+            is_type = (
+                is_type
+                or t in types
+                or (is_valid and isinstance(t, types))
+                or (is_valid and issubclass(t, types))  # type: ignore
+            )
+
+        return is_type
+
+    @property
+    def model_type(self) -> Type[BaseModel]:
+        """Try to return the `pydantic.BaseModel` type.
+
+        Raises:
+            TypeError: if this field is not a `pydantic.BaseModel` or if the model type cannot be found.
+        """
+        if not self.is_a(BaseModel):
+            raise TypeError("This `pydantic` field is not a `pydantic.BaseModel`")
+
+        types = self.get_type()
+        if not isinstance(types, tuple):
+            return cast(Type[BaseModel], types)
+
+        for t in types:
+            if isinstance(t, type) and issubclass(t, BaseModel):
+                return t
+        else:
+            raise TypeError(
+                "No `pydantic.BaseModel`s were found associated with this field."
+            )
+
+    def is_subcommand(self) -> bool:
+        """Check whether the input pydantic Model is a subcommand.
+
+        The default is that all pydantic Models are not subcommands, so this
+        has this featured has to be opted in by adding `subcommand=True`
+        to the `json_schema_extra` model config. A convenience class has been created
+        to provide this default for models that are meant to be command switches at
+        the command line: `pydantic_argparse.BaseCommand`.
+
+        Returns:
+            bool: if the pydantic model is a subcommand. In all other cases, including when this field is not a
+                pydantic model, returns False.
+        """
+        default = False
+        try:
+            model = self.model_type
+            value = model.model_config["json_schema_extra"].get("subcommand", default)  # type: ignore
+            return cast(bool, value)
+        except (KeyError, AttributeError, TypeError):
+            # KeyError if:
+            #   - subcommand key not in json_schema_extra
+
+            # AttributeError if:
+            #   - json_schema_extra not in the model_config, ie if using BaseModel
+            # just default to not being a subcommand
+
+            # TypeError if
+            #   - field is not a pydantic BaseModel or it can't be found
+            return default
 
 
 def as_validator(
@@ -121,9 +275,9 @@ def update_validators(
 
 
 def model_with_validators(
-    model: Type[PydanticModelT],
+    model: Type[BaseModel],
     validators: Dict[str, PydanticValidator],
-) -> Type[PydanticModelT]:
+) -> Type[BaseModel]:
     """Generates a new `pydantic` model class with the supplied validators.
 
     If the supplied base model is a subclass of `pydantic.BaseSettings`, then
@@ -133,11 +287,11 @@ def model_with_validators(
     passed through to the `pydantic` field validators if initial parsing fails.
 
     Args:
-        model (Type[PydanticModelT]): Model type to use as base class.
+        model (Type[BaseModel]): Model type to use as base class.
         validators (Dict[str, PydanticValidator]): Field validators to add.
 
     Returns:
-        Type[PydanticModelT]: New `pydantic` model type with field validators.
+        Type[BaseModel]: New `pydantic` model type with field validators.
     """
     # Construct New Model with Validators
     model = pydantic.create_model(
